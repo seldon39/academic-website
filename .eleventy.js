ae5399f61d5ee4af6c6e35eed196a4f453814b12
seldon39 <92303924+seldon39@users.noreply.github.com>
2026-08-12 17:21:08 +0200

    Fix : Optimisation du favicon pour la barre de favoris et les onglets

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
-  eleventyConfig.addPassthroughCopy("src/assets/favicon.png");
+  eleventyConfig.addPassthroughCopy("src/assets/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/assets/*.png");
   return {
     dir: {
       input: "src",          // 11ty va maintenant lire les pages dans src/
